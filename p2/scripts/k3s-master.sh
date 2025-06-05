0c6350d2948cfb572800d25dd24893ef0a6b7634
Jeremy Maia <jmaia@student.42.fr>
2025-06-05 21:20:18 +0200

    wip

--- p2/scripts/k3s-master.sh
+++ p2/scripts/k3s-master.sh
@@ -6,3 +6,7 @@ curl -sfL https://get.k3s.io | sh -
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
