79dbbf707e0532a39280911c39e6966f5f1ed9d7
Jeremy Maia <jmaia@student.42.fr>
2025-06-05 20:31:15 +0200

    wip

--- p2/scripts/k3s-master.sh
+++ p2/scripts/k3s-master.sh
@@ -6,9 +6,3 @@ curl -sfL https://get.k3s.io | sh -
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
-
-# Get the token for the worker nodes
-TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
-
-# Store the token for the workers to use
-echo $TOKEN > /vagrant/token
