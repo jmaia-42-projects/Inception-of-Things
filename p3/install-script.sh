ed6a88f92b064c35a05cb73002465700b287d9a8
Jeremy Maia <jmaia@student.42.fr>
2025-06-05 22:04:56 +0200

    wip

--- p3/install-script.sh
+++ p3/install-script.sh
@@ -15,3 +15,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo k3d cluster create
+sudo ./kubectl create namespace argocd
+sudo ./kubectl create namespace dev
+
+sudo ./kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
